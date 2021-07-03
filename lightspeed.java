f764e248dcb16142bc7416645e85d41f41962cd4
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-07-03 13:13:51 +0530

    Add files via upload

---
+++ lightspeed.java
@@ -0,0 +1,20 @@
+package fibonacciseries;
+
+public class lightspeed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int lightspeed;long days,seconds,distance;
+//approximate speed of light in miles per second
+lightspeed=186000;days=1000;//specify number of days here 
+seconds=days*24*60*60;
+//convert to seconds
+distance=lightspeed*seconds;
+//compute distance
+System.out.print("In"+days);
+System.out.print(" days light will travel about ");
+System.out.println(distance+"miles.");}
+
+	}
+
+
